9b81e2bc450af4afa6431c66ee33e9320aecefcb
M'hamed JAMIL <mhamed.jamil@gmail.com>
2026-04-18 23:21:23 +0100

    Initial commit

---
+++ src/lib/code-generator.js
@@ -0,0 +1,19 @@
+import 'picoblock-blocks/arduino_compressed';
+import 'picoblock-blocks/python_compressed';
+
+import {DeviceType} from './device';
+
+const getGeneratorNameFromDeviceType = deviceType => {
+    if (deviceType === DeviceType.arduino) {
+        return 'Arduino';
+    } else if (deviceType === DeviceType.python ||
+        deviceType === DeviceType.microPython ||
+        deviceType === DeviceType.microbit) {
+        return 'Python';
+    }
+    return 'null';
+};
+
+export {
+    getGeneratorNameFromDeviceType
+};
